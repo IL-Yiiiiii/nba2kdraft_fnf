2d38d10be3fa2cb6a3afa3c9cb6331aa9e8373cd
IL_Yiiiiii <144751883+IL-Yiiiiii@users.noreply.github.com>
2026-07-01 22:39:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import os
 import pickle
 
 DB_FILE = "draft_backup.pkl"
-if os.path.exists("draft_backup.pkl"):
-    os.remove("draft_backup.pkl")
 def save_draft_state(state_dict):
     """Saves the current shared_draft state to a physical file."""
     with open(DB_FILE, "wb") as f:
